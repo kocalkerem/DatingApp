0651b5326380cf4bc9a6fe4264378f8cbfab9dbf
kocalkerem <kocalkerem@gmail.com>
2020-06-10 00:22:31 +0300

    end of section 6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 
 @Component({
@@ -11,15 +12,15 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
  @Output() cancelRegister = new EventEmitter();
  model: any = {}; 
-  constructor(private authService: AuthService ) {}
+  constructor(private authService: AuthService , private alertify: AlertifyService ) {}
 
   ngOnInit() {} 
 
   register() {
        this.authService.register(this.model).subscribe(() => {
-         console.log('registration successfull');
+         this.alertify.success('registration successfull');
        }, error => {
-         console.log(error);
+         this.alertify.error(error);
        });
   }
 
